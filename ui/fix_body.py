a8b6bf2914831f0058bc668f66d18af4c3f68bfb
ezTxmMC <tom.handrick@vesperia.net>
2026-04-06 03:14:28 +0200

    upgraded dependencies

--- ui/fix_body.py
+++
@@ -1,5 +0,0 @@
-with open('src/assets/base.css', 'r') as f:
-    text = f.read()
-text = text.replace('.body { flex: 1;  overflow: hidden; position: relative; z-index: 10; }', '.body { flex: 1; display: flex; flex-direction: column; overflow: hidden; position: relative; z-index: 10; }')
-with open('src/assets/base.css', 'w') as f:
-    f.write(text)
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import VueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss()],
+  plugins: [vue(), vueJsx(), VueDevTools(), tailwindcss()],
   base: "./",
   resolve: {
     alias: {
